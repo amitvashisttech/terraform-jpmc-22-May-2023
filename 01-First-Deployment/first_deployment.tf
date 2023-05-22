7a4b12de0c7c364a3e65ac61a8ff0ca23c504733
Amit Vashist <amitvashist7@outlook.com>
2023-05-22 09:31:08 +0000

    01-First-Deployment

--- 01-First-Deployment/first_deployment.tf
+++ 01-First-Deployment/first_deployment.tf
@@ -9,7 +9,9 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
-
+  root_block_device {
+    volume_type   = "gp2"
+  }
   tags = {
     Name = "HelloWorld"
   }
